b24c22f24cfad06b5b9b3a765bc832dcb638e649
Lisa Jo <lisaxjo@gmail.com>
2019-10-23 12:29:24 -0400

    chore: removed yarn.lock file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
-import logo from './logo.svg';
-import './App.css';
 import Home from "./components/Home"
 import Stem from "./components/stem"
 import Contact from "./components/Contact"
